84c6d601f4d295df20a40725f51a60d31659b14f
tengxiao.ma <tengxiao.ma@huifu.com>
2020-04-21 14:30:31 +0800

    项目初始化

---
+++ src/main/java/com/mtx/kyrieboot/KyriebootApplication.java
@@ -0,0 +1,13 @@
+package com.mtx.kyrieboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyriebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyriebootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtx/kyrieboot/KyriebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtx.kyrieboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyriebootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
