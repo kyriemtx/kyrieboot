07bd8ca89a021c74e21d720420c1d9e451585891
tengxiao.ma <tengxiao.ma@huifu.com>
2020-04-28 11:11:46 +0800

    提交

---
+++ src/main/java/com/mtx/kyrieboot/controller/BuildController.java
@@ -0,0 +1,22 @@
+package com.mtx.kyrieboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName BuildController
+ * @Description
+ * @Author tengxiao.ma
+ * @Date 2020/4/27 10:55
+ **/
+
+@Controller
+@RequestMapping("/build")
+public class BuildController {
+
+    @GetMapping("/build")
+    public String build(){
+        return "module/build/build";
+    }
+}
--- src/main/resources/static/js/modules/menu/updateMenu.js
+++ src/main/resources/static/js/modules/menu/updateMenu.js
@@ -49,7 +49,6 @@ var app = new Vue({
                         parent.layer.alert("Connection error");
                     },
                     success: function (data) {
-                        debugger
                         if (data.respCode == 200) {
                             $("#menuNames").show();
                             $("#menuNames").html("");
