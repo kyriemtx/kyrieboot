3cdca33b2ea42de1e39a4672c2a262f5da04a2c1
tengxiao.ma <tengxiao.ma@huifu.com>
2020-04-29 10:20:15 +0800

    修改部署方式

--- src/main/java/com/mtx/kyrieboot/KyriebootApplication.java
+++ src/main/java/com/mtx/kyrieboot/KyriebootApplication.java
@@ -2,18 +2,9 @@ package com.mtx.kyrieboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KyriebootApplication extends SpringBootServletInitializer {
-
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(KyriebootApplication.class);
-    }
-
+public class KyriebootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KyriebootApplication.class, args);
